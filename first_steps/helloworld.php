674d0fba86eba83b114a414e464bed31a0701742
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-05-29 10:14:16 +0200

    First commit

---
+++ first_steps/helloworld.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<title>Hello World!</title>
+	</head>
+	<body>
+	<?php 
+        // signle line comments are like this - more common
+        # or like this
+        /* double line comment here
+        and we can keep typing
+        and typing
+        */
+    echo "Hello World!<br />"; ?>
+	<?php print "Hello World!"; ?><br />
+	<?php echo "Hello" . " World!"; ?><br />
+	<?php echo 2+3; ?><br />
+	</body>
+</html> 

1d9ddf9312dd8e89e21b1d5a6bd263908ace8cf7
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-05-29 14:40:01 +0200

    Added Logical Expression files

---
+++ control_structures_logical_expressions/logical.php
@@ -0,0 +1,48 @@
+<html>
+    <head>
+        <title>Logical Expressions</title>
+    </head>
+    <body>
+       <?php 
+            $a = 5;
+            $b = 4;
+            if ($a > $b) {
+                echo "a is larger than b";
+            } else if ($a == $b) {
+                echo "a is equal to b";
+            } else {
+                echo "a is smaller than b";
+            }
+       ?>
+       <br>
+       <?php
+            $c = 1;
+            $d = 20;
+            if (($a > $b) && ($c > $d)) {
+                echo "a is larger than b AND ";
+                echo "c is larger than d";
+            }
+            if (($a > $b) || ($c > $d)) {
+                echo "a is larger than b OR ";
+                echo "c is larger than d";
+            } else {
+                echo "neither a is larger than b or c is larger than d";
+            }
+       ?>
+       <br>
+       <?php 
+            unset($a);
+            if (!isset($a)) {
+                $a = 100;
+            }
+            echo $a;
+       ?>
+       <br>
+       <?php 
+            if (is_int($a)) {
+                settype($a, "string");
+            }
+            echo gettype($a);
+       ?>
+    </body>
+</html>

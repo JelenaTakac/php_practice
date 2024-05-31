91098101390c2a46ee60243724ad107096019d63
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-05-31 11:04:45 +0200

    Checking PHP version

--- building_web_pages/firstpage.php
+++ building_web_pages/firstpage.php
@@ -1,12 +1,17 @@
 <html>
-    <head>
-        <title>First Page</title>
-    </head>
-    <body>
-        <?php 
-            $linktext = "<Click> & you'll see";
-        ?>
-        <a href="secondpage.php?name=<?php echo urlencode("kevin skoglund"); ?>&id=42"><?php echo htmlspecialchars($linktext); ?></a>
-        
-    </body>
+
+<head>
+    <title>First Page</title>
+</head>
+
+<body>
+    <?php
+    $linktext = "<Click> & you'll see";
+    ?>
+    <a href="secondpage.php?name=<?php echo urlencode("kevin skoglund"); ?>&id=42"><?php echo htmlspecialchars($linktext); ?></a>
+
+    <?php echo phpversion(); ?>
+
+</body>
+
 </html>
